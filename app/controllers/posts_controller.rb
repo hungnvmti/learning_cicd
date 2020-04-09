e7e9b96d15e3b906acd34261d335fd47e73437d9
Nguyen Van Hung <hungnv@mti-tech.vn>
2020-04-09 16:31:27 +0700

    update cicd

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,16 @@
 class PostsController < ApplicationController
   def index
   	total = sum 1,1 
+    Post.create(name: "David", description: "Code Artist")
+    post = Post.first
   	@message = "Hello "+ post.name.to_s + ", how are you today? Could you help me check the results? it's correct or not: 1 + 1 = " + total.to_s
     #@message = "Hello, how are you today? Could you help me check the results? it's correct or not: 1 + 1 = " + total.to_s
   
   end
 
-  def post
-  	@post ||= Post.create(name: "David", description: "Code Artist")
-  end
+  # def post
+  # 	@post ||= Post.create(name: "David", description: "Code Artist")
+  # end
 
   def sum (a,b)
   	total = a + b
