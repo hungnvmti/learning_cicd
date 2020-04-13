edb9f88b2c8aa9dc295e7959b5ebab6b6b052299
Nguyen Van Hung <hungnv@mti-tech.vn>
2020-04-13 20:06:57 +0700

    fix bug

--- app/controllers/greeting_controller.rb
+++ app/controllers/greeting_controller.rb
@@ -10,12 +10,9 @@ class GreetingController < ApplicationController
     end
 
   	total = sum 1,1 
-    Greeting.create(title: "HungNV", description: "Code Artist")
 
-    #greeting = Greeting.first
-  	@message = "Hello "+ greeting.title.to_s + ", how are you today? Could you help me check the results? it's correct or not: 1 + 1 = " + total.to_s
-    # @message = "Hello, how are you today? Could you help me check the results? it's correct or not: 1 + 1 = " + total.to_s
-  
+    greeting = Greeting.first
+  	@message = "Hello #{greeting.title.to_s}, how are you today? Could you help me check the results? it's correct or not: 1 + 1 = #{total.to_s}"  
   end
 
   # def greeting
