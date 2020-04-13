a20cef53461cfacdf3d3e2a639aee68aea038e2d
Nguyen Van Hung <hungnv@mti-tech.vn>
2020-04-13 20:38:21 +0700

    log sql

--- app/controllers/greeting_controller.rb
+++ app/controllers/greeting_controller.rb
@@ -3,8 +3,7 @@ class GreetingController < ApplicationController
     begin
       ActiveRecord::Base.establish_connection # Establishes connection
       ActiveRecord::Base.connection # Calls connection object
-      puts "CONNECTED!" if ActiveRecord::Base.connected? 
-      puts ActiveRecord::Base.connection.tables
+      puts "CONNECTED! #{ActiveRecord::Base.connection.tables}" if ActiveRecord::Base.connected? 
       puts "NOT CONNECTED!" unless ActiveRecord::Base.connected?
     rescue
       puts "NOT CONNECTED!"
@@ -13,7 +12,7 @@ class GreetingController < ApplicationController
   	total = sum 1,1 
 
    # greeting = Greeting.first.to_sql
-  	@message = "Hello #{greeting.to_s}, how are you today? Could you help me check the results? it's correct or not: 1 + 1 = #{total.to_s}"  
+  	@message = " how are you today? Could you help me check the results? it's correct or not: 1 + 1 = #{total.to_s}"  
   end
 
   # def greeting
