9bac0c71d6bbcb2eb1d83e301d404d1c7eaa31ae
Nguyen Van Hung <hungnv@mti-tech.vn>
2020-04-14 19:23:58 +0700

    add login heroku

--- .circleci/setup-heroku.sh
+++ .circleci/setup-heroku.sh
@@ -3,8 +3,10 @@ set -eux
 curl https://cli-assets.heroku.com/install-ubuntu.sh | sh
 heroku --version
 git --version
-# heroku git:remote -a m1k
-git push https://heroku:d6eb3953-ecaf-4a11-bffd-5db7c4e629ce@git.heroku.com/m1k.git master
+git remote -v
+heroku login -i
+heroku git:remote -a m1k
+git push heroku master
 heroku run rake db:migrate
 sleep 5 # sleep for 5 seconds to wait for dynos
 heroku restart
