d1a06f91596ca65241bd8716b7f5436f3aa31576
Nguyen Van Hung <hungnv@mti-tech.vn>
2020-04-14 18:30:50 +0700

    config heroku

--- .circleci/setup-heroku.sh
+++ .circleci/setup-heroku.sh
@@ -1,4 +1,3 @@
-git push heroku sequential-branch-filter:master
-heroku run rake db:migrate
-sleep 5 # sleep for 5 seconds to wait for dynos
-heroku restart
+#!/bin/bash
+set -eux
+curl https://cli-assets.heroku.com/install-ubuntu.sh | sh
