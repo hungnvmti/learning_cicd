97d808be63a1efb428d8791aa6f7239a07eb9729
Nguyen Van Hung <hungnv@mti-tech.vn>
2020-04-14 18:56:35 +0700

    using OAuth push heroku master

--- .circleci/setup-heroku.sh
+++ .circleci/setup-heroku.sh
@@ -3,4 +3,8 @@ set -eux
 curl https://cli-assets.heroku.com/install-ubuntu.sh | sh
 heroku --version
 git --version
-heroku git:remote -a m1k
+# heroku git:remote -a m1k
+git push https://heroku:d6eb3953-ecaf-4a11-bffd-5db7c4e629ce@git.heroku.com/m1k.git master
+heroku run rake db:migrate
+sleep 5 # sleep for 5 seconds to wait for dynos
+heroku restart
